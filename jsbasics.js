1fc085195388c6fc0ffea3b3b90f59a6424da001
heetfx <makwanaheet7@gmail.com>
2025-11-01 11:49:25 +0530

    jsbasic

---
+++ jsbasics.js
@@ -0,0 +1,21 @@
+//console.log("hello world");
+
+let namee = "heet";
+let age = 18;
+
+// console.log(namee)
+// console.log(age)
+
+// console.log(namee,age);
+// console.table([namee,age]);
+
+// console.log(typeof namee)
+
+// console.log(typeof age)
+
+let a = 2;
+let b ="2";
+console.log(a ==b);  // in java we do triple = to check logically
+                     //== for value , === for type
+console.log(a ===b);
+
